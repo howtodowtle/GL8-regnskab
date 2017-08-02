b015fb00624c6829bedba2f8483e893ec9ede927
Alexander Jakob Dautel <howtodowtle@users.noreply.github.com>
2017-08-03 00:26:40 +0200

    dothestuff.py added

---
+++ dothestuff.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  3 00:24:13 2017
+
+@author: howtodowtle
+"""
+
+import gorgireader
+import gorgimessages
+import opslaget
+
+# only define path when different than 
+# D:/Dropbox/Egmont/Regnskab/Aktuelt Regnskab/gangregnskab_egmontgl8.xlsx
+df = gorgireader.getWorkbook()
+month = "juli"
+year = "2017"
+# dates format: "2/7/2017-2/8/2017", or however you would like it to look
+dates = "2/7/2017-2/8/2017"
+deadline = "17/8"
+
+# so far only printing, should also be saved in future
+# create fb posting
+opslaget.createOpslagetDK(month, year, dates)
+
+# create messages
+gorgimessages.createAllMessages(df, month, year, dates, deadline)
